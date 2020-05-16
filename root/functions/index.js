ed3024d577567ce043e0dd04d29aca646134bb7a
JonahS1 <JonahS1@users.noreply.github.com>
2020-05-16 19:49:11 -0400

    add new users' email addresses to firestore

--- root/functions/index.js
+++ root/functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+var db = admin.firestore();
+// var db = firebase.firestore();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.addUserToDB = functions.auth.user().onCreate((user) => {
+    db.collection("students").add({
+        email: user.email
+    })
+    .then((docRef) => {
+        return console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+});
