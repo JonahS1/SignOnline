7f4a92553e6502c08230e62cdf43839edd03b553
ZackSussman <65458956+ZackSussman@users.noreply.github.com>
2020-05-17 03:11:52 -0400

    add sendInvite()

--- root/public/js/scripts.js
+++ root/public/js/scripts.js
@@ -1,5 +1,7 @@
 var db = firebase.firestore();
 var userEmail = JSON.parse(sessionStorage.userEmail);
+
+
 var sendRequest = () => {
 
     db.collection('students').where("email", "==", document.getElementById("reqEmail").value).get()
@@ -23,5 +25,27 @@ var sendRequest = () => {
     });
 };
 
+var sendInvite = () => {
+
+    db.collection('students').where("email", "==", userEmail).get()
+    .then(querySnapshot => {
+        var doc = querySnapshot.docs[0];
+        console.log(querySnapshot.docs);
+        db.collection('students').doc(doc.id).collection('sigs').add({
+            authorEmail: document.getElementById("invEmail").value,
+            svgString: '',
+            access: true
+        })
+        .then(function(docRef){
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error){
+            console.error("Error adding document: ", error);
+        });
+    })
+    .catch(function(error){
+        console.error("Error with the QUERY: ", error);
+    });
+};
 
 
