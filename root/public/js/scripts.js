8d11a62c75a4c98367559821d40dadca8c029433
ZackSussman <65458956+ZackSussman@users.noreply.github.com>
2020-05-18 07:03:24 -0400

    fixed invalid email errors and problems with doesEmailExist function

--- root/public/js/scripts.js
+++ root/public/js/scripts.js
@@ -4,6 +4,7 @@ var userEmail = JSON.parse(sessionStorage.userEmail);
 
 var sendRequest = () => {
 
+var sendRequestSuccessful = () => {
     db.collection('students').where("email", "==", document.getElementById("reqEmail").value).get()
     .then(querySnapshot => {
         document.getElementById("reqEmail").value = "Success";
@@ -26,115 +27,128 @@ var sendRequest = () => {
         document.getElementById("reqEmail").value = "not a valid email!";        
         console.error("Error with the QUERY: ", error);
     });
+}
+
+var sendRequestFailure = () => {
+    document.getElementById('reqEmail').value = "invalid email";
+}
+
+var sendRequest = () => {
+    doesUserExistInstruction(document.getElementById('reqEmail').value, sendRequestSuccessful, sendRequestFailure);
 };
 
 
 
+var sendInviteSuccessful = () => {
+    db.collection('students').where("email", "==", userEmail).get()
+                    .then(querySnapshot => {
+                        var doc = querySnapshot.docs[0];
+                        console.log(querySnapshot.docs);
+                        db.collection('students').doc(doc.id).collection('sigs').add({
+                            authorEmail: document.getElementById("invEmail").value,
+                            svgString: '',
+                            access: true,
+                            request: false
+                        })
+                        .then(function(docRef){
+                            console.log("Document written with ID: ", docRef.id);
+                        })
+                        .catch(function(error){
+                            console.error("Error adding document: ", error);
+                        });
+                    })
+                    .catch(function(error){
+                        console.error("Error with the QUERY: ", error);
+                    });
+                    //send invite to person
+                    db.collection('students').where("email", "==", document.getElementById("invEmail").value ).get()
+                    .then(querySnapshot => {
+                        var doc = querySnapshot.docs[0];
+                        console.log(querySnapshot.docs);
+                        db.collection('students').doc(doc.id).collection('sigs').add({
+                            authorEmail: document.getElementById("invEmail").value,
+                            senderEmail: userEmail,
+                            svgString: '',
+                            access: true,
+                            request: false
+                        })
+                        .then(function(docRef){
+                            console.log("Document written with ID: ", docRef.id);
+                        })
+                        .catch(function(error){
+                            console.error("Error adding document: ", error);
+                        });
+                        document.getElementById("invEmail").value = "Success";
+                    })
+                    .catch(function(error){
+                        console.error("Error with the QUERY: ", error);
+                    });
+}
+
+var sendInviteFailure = () => {
+    document.getElementById('invEmail').value = "invalid email";
+}
 
 
 var sendInvite = () => {
-    //if (doesUserExist()) {
-    //send invite to self   
-    db.collection('students').where("email", "==", userEmail).get()
-        .then(querySnapshot => {
-            var doc = querySnapshot.docs[0];
-            console.log(querySnapshot.docs);
-            db.collection('students').doc(doc.id).collection('sigs').add({
-                authorEmail: document.getElementById("invEmail").value,
-                svgString: '',
-                access: true,
-                request: false
-            })
-            .then(function(docRef){
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function(error){
-                console.error("Error adding document: ", error);
-            });
-        })
-        .catch(function(error){
-            console.error("Error with the QUERY: ", error);
-        });
-        //send invite to person
-        db.collection('students').where("email", "==", document.getElementById("invEmail").value ).get()
-        .then(querySnapshot => {
-            var doc = querySnapshot.docs[0];
-            console.log(querySnapshot.docs);
-            db.collection('students').doc(doc.id).collection('sigs').add({
-                authorEmail: document.getElementById("invEmail").value,
-                senderEmail: userEmail,
-                svgString: '',
-                access: true,
-                request: false
-            })
-            .then(function(docRef){
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function(error){
-                console.error("Error adding document: ", error);
-            });
-            document.getElementById("invEmail").value = "Success";
-        })
-        .catch(function(error){
-            console.error("Error with the QUERY: ", error);
-        });
-    //}
-    //else {
-     //   document.getElementById("invEmail").value = "not a valid email!";
-    //}
+       doesUserExistInstruction(document.getElementById('invEmail').value, sendInviteSuccessful, sendInviteFailure);
 };
 
-var makeIncomingRequestList = () => {
-    document.getElementById('inReqList').innerHTML = "";
-    db.collection('students').where("email", "==", userEmail).get()
-    .then(querySnapshot => {
-        var doc = querySnapshot.docs[0];
-        db.collection('students').doc(doc.id).collection('sigs').where("request", "==", true).get()
-        .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                document.getElementById('inReqList').innerHTML += `<br>${doc.get('authorEmail')}  <i class="material-icons green-text" id="${doc.get('authorEmail')}" onClick='acceptRequestOfEmail("${doc.get('authorEmail')}")'>check</i>  <i class="material-icons red-text" id="${doc.get('authorEmail')}" onClick='rejectRequestOfEmail("${doc.get('authorEmail')}")'>clear</i>`;
+        var makeIncomingRequestList = () => {
+            document.getElementById('inReqList').innerHTML = "";
+            db.collection('students').where("email", "==", userEmail).get()
+            .then(querySnapshot => {
+                var doc = querySnapshot.docs[0];
+                db.collection('students').doc(doc.id).collection('sigs').where("request", "==", true).get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        document.getElementById('inReqList').innerHTML += `<br>${doc.get('authorEmail')}  <i class="material-icons green-text" id="${doc.get('authorEmail')}" onClick='acceptRequestOfEmail("${doc.get('authorEmail')}")'>check</i>  <i class="material-icons red-text" id="${doc.get('authorEmail')}" onClick='rejectRequestOfEmail("${doc.get('authorEmail')}")'>clear</i>`;
+                    })
+                })
+                .catch(function(error){    
+                    console.error("Error with the QUERY: ", error);
+                });
             })
-        })
-        .catch(function(error){    
-            console.error("Error with the QUERY: ", error);
-        });
-    })
-    .catch(function(error){     
-        console.error("Error with the QUERY: ", error);
-    });
-};
-
-var makeIncomingInviteList = () => {
-    document.getElementById('inInvList').innerHTML = "";
-    db.collection('students').where("email", "==", userEmail).get()
-    .then(querySnapshot => {
-        var doc = querySnapshot.docs[0];
-        db.collection('students').doc(doc.id).collection('sigs').where("authorEmail", "==", userEmail).get()
-        .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                document.getElementById('inInvList').innerHTML += `<br>${doc.get('senderEmail')}  <i class="material-icons green-text" id="${doc.get('senderEmail')}" onClick='prepareForDrawingPage("${doc.get('senderEmail')}")'>edit</i>  <i class="material-icons red-text" id="${doc.get('senderEmail')}" onClick='rejectRequestOfEmail("${doc.get('senderEmail')}")'>clear</i>`;
+            .catch(function(error){     
+                console.error("Error with the QUERY: ", error);
             });
+        };
+
+        var makeIncomingInviteList = () => {
+            document.getElementById('inInvList').innerHTML = "";
+            db.collection('students').where("email", "==", userEmail).get()
+            .then(querySnapshot => {
+                var doc = querySnapshot.docs[0];
+                db.collection('students').doc(doc.id).collection('sigs').where("authorEmail", "==", userEmail).get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        document.getElementById('inInvList').innerHTML += `<br>${doc.get('senderEmail')}  <i class="material-icons green-text" id="${doc.get('senderEmail')}" onClick='prepareForDrawingPage("${doc.get('senderEmail')}")'>edit</i>  <i class="material-icons red-text" id="${doc.get('senderEmail')}" onClick='rejectRequestOfEmail("${doc.get('senderEmail')}")'>clear</i>`;
+                    });
+                })
+                .catch(function(error){     
+                    console.error("Error with the QUERY: ", error);
+                });
+            })
+            .catch(function(error){ 
+                console.error("Error with the QUERY: ", error);
+            });
+        }
+
+
+function doesUserExistInstruction(email, instruction, other) {
+    userExists = false;
+    db.collection('students').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            if (doc.get("email").toUpperCase() == email.toUpperCase()) {
+                instruction();
+            }
         })
-        .catch(function(error){     
-            console.error("Error with the QUERY: ", error);
-        });
+        other();
     })
     .catch(function(error){ 
-        console.error("Error with the QUERY: ", error);
+        console.error("Error with the search: ", error);
     });
-
-}
-
-
-function doesUserExist(email) {
-    db.collection('students').where("email", "==", email).get()
-   .then(querySnapshot => {
-        return querySnapshot.docs.length > 0;
-   })
-   .catch(function(error){
-    console.error("Error with the Query: ", error);
-    });
-}
+};
 
 
 function removeInvitationFromEmail(email) {
@@ -242,8 +256,6 @@ function removeInvitationFromEmail(email) {
 
 
 function prepareForDrawingPage(email) {
-    
     sessionStorage.senderEmail = email;
     window.open('../html/canvas.html')
-
 }
